f447fa13387a03a04f3cb272143c6fb16167641b
Rajkumar <raj345059@gmail.com>
2025-04-26 23:00:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -345,8 +345,8 @@ def get_layout(tool):
     user_name = st.session_state.user_name.title()
     # logout_sidebar(user_name)
     output_dict = get_prompt(tool, user_name)
-    st.title(output_dict['title'])
-    output_dict['header']
+    # st.title(output_dict['title'])
+    # output_dict['header']
     query = st.chat_input(placeholder="Write your query?")
     if "messages" not in st.session_state:
         st.session_state["messages"]=[]
